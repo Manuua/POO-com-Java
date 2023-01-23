2a2c56e462a23595b456c58ec5a5f027451d1ebb
Manoela Araújo <91172005+Manuua@users.noreply.github.com>
2023-01-23 19:10:16 -0300

    feat: exercício  com encapsulamento

---
+++ Aula06/src/main/java/aula/aula06/Aula06.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package aula.aula06;
+
+/**
+ *
+ * @author manoela.araujo
+ */
+public class Aula06 {
+
+    public static void main(String[] args) {
+       ControleRemoto c = new ControleRemoto();
+
+       c.maisVolume();
+       c.play();
+       c.abrirMenu();
+       c.fecharMenu();
+       
+    }
+}
